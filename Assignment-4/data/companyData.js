25aa19ee3e4639ae0bb18cf456a6b5fc6fdfd5dd
MansiGodhani <81357299+MansiGodhani@users.noreply.github.com>
2021-12-04 13:33:14 +0530

    Add files via upload

---
+++ Assignment-4/data/companyData.js
@@ -0,0 +1,8 @@
+const company = [
+    {
+        "companyId": "1",
+        "name": "Apple",
+        "productId" : ["101"]
+    }
+];
+module.exports = company;
---
+++ Assignment-4/data/sellerData.js
@@ -0,0 +1,14 @@
+const seller = [
+    {
+        "sellerId": "1",
+        "name": "Jack",
+        "productId" : ["101"]
+    },
+    {
+        "sellerId": "2",
+        "name": "Jack",
+        "productId" : ["102"]
+    }
+];
+
+module.exports = seller;
